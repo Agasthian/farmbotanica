9855999a0b755e5d305fdb5c4592601e3b411014
Agasthian <agasthian_r@hotmail.com>
2020-08-16 22:58:16 +0530

    Compelted setup of styled components themes and mdx with grapghql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,53 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `poppins`,
+          `open sans`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
+  ],
 }
---
+++ src/themes/theme.js
@@ -0,0 +1,41 @@
+export default {
+  fonts: {
+    main: "Poppins, sans-serif",
+    para: "Open Sans, sans-serif",
+  },
+  colors: {
+    main1: "green",
+    main2: "lemonyellow",
+    dark1: "hsl(227,2%,12%)",
+    dark2: "hsl(227,2%,26%)",
+    dark3: "hsl(227,2%,64%)",
+    light1: "hsl(0,0%,97%)",
+    light2: "hsl(0,0%,99%)",
+  },
+  mediaQueries: {
+    micro: `only screen and (max-width:20em)`,
+    smallest: `only screen and (max-width: 25em)`,
+    smaller: "only screen and (max-width: 34.37em)", // 550px
+    small: "only screen and (max-width: 48em)", //768px
+    medium: "only screen and (max-width: 56.25em)", //900px
+    large: "only screen and (max-width: 62.5em)", //1000px
+    larger: "only screen and (max-width: 80em)",
+    largest: "only screen and (max-width: 90em)",
+  },
+  spacings: {
+    xxSmall: ".25em",
+    xSmall: ".5em",
+    Small: "1em",
+    medium: "2em",
+    large: "3em",
+    xLarge: "4em",
+    xxLarge: "6em",
+  },
+  animation: {
+    button: "box-shadow 0.3 ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "0px 5px 20px rgba(30,30,31,0.5)",
+  },
+}
